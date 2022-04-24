2ff4f3683887948d83136221a5cc7e7cfc961529
a7medF4 <a7med454542@gmail.com>
2022-04-24 19:33:20 +0200

    2

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ let products = [
     desc: "Doe is very bad and very nice",
     Image: "/images/1.jpg",
     Image2: "/images/2.jpg",
-    Image3: "/images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+    Image3: "/gam3a/images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
   },
   {
     title: "John",
