f451aa4e33b4db70cedcfc5254782a2c9d90d5e8
a7medF4 <a7med454542@gmail.com>
2022-04-24 16:59:55 +0200

    comments

---
+++ main.js
@@ -0,0 +1,112 @@
+let products = [
+  {
+    title: "John",
+    desc: "Doe is very bad and very nice",
+    Image: "/images/1.jpg",
+    Image2: "/images/2.jpg",
+    Image3: "/images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+  },
+   {
+    title: "John",
+    desc: "Doe is very bad and very nice",
+    Image: "/images/1.jpg",
+    Image2: "/images/2.jpg",
+    Image3: "/images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+  },
+  {
+    title: "John",
+    desc: "Doe is very bad and very nice",
+    Image: "/images/1.jpg",
+    Image2: "/images/2.jpg",
+    Image3: "/images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+  },
+  {
+    title: "John",
+    desc: "Doe is very bad and very nice",
+    Image: "/images/1.jpg",
+    Image2: "/images/2.jpg",
+    Image3: "/images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+  },
+  {
+    title: "John",
+    desc: "Doe is very bad and very nice",
+    Image: "/images/1.jpg",
+    Image2: "/images/2.jpg",
+    Image3: "/images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+  },
+  {
+    title: "John",
+    desc: "Doe is very bad and very nice",
+    Image: "/images/1.jpg",
+    Image2: "/images/2.jpg",
+    Image3: "/images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+  },
+  {
+    title: "John",
+    desc: "Doe is very bad and very nice",
+    Image: "/images/1.jpg",
+    Image2: "/images/2.jpg",
+    Image3: "/images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+  },
+  {
+    title: "John",
+    desc: "Doe is very bad and very nice",
+    Image: "/images/1.jpg",
+    Image2: "/images/2.jpg",
+    Image3: "/images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+  },
+  {
+    title: "John",
+    desc: "Doe is very bad and very nice",
+    Image: "/images/1.jpg",
+    Image2: "/images/2.jpg",
+    Image3: "/images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+  },
+  {
+    title: "John",
+    desc: "Doe is very bad and very nice",
+    Image: "/images/1.jpg",
+    Image2: "/images/2.jpg",
+    Image3: "/images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+  },
+  {
+    title: "John",
+    desc: "Doe is very bad and very nice",
+    Image: "/images/1.jpg",
+    Image2: "/images/2.jpg",
+    Image3: "/images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+  },
+  {
+    title: "John",
+    desc: "Doe is very bad and very nice",
+    Image: "/images/1.jpg",
+    Image2: "/images/2.jpg",
+    Image3: "/images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+  },
+  {
+    title: "John",
+    desc: "Doe is very bad and very nice",
+    Image: "/images/1.jpg",
+    Image2: "/images/2.jpg",
+    Image3: "/images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+  },
+];
+function showDate(){
+    let table = '';
+    for(let i =0; i < products.length;i++){
+        table += `
+            <div>
+                <img src="${products[i].Image3}" id="image" alt="Sample photo">
+                <div class="text">
+                  <h3>${products[i].title}</h3>
+                  <p>${products[i].desc}</p>
+                  <a href="wow.html">more</a>
+                </div> 
+           </div>
+        `
+    }           
+    document.getElementById('id').innerHTML = table;
+
+}
+showDate()
+
