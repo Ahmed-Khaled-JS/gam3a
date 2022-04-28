2074dca9926a084c795dd2076830b658170b2971
a7medF4 <a7med454542@gmail.com>
2022-04-28 02:52:55 +0200

    mouse

--- main.js
+++ main.js
@@ -26,9 +26,10 @@ let products = [
   {
     title: "mohamed",
     desc: "Doe is very bad and very nice",
-    Image: "images/1.jpg",
-    Image2: "images/2.jpg",
-    Image3: "images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+    Image: "store_images/f0924c964ace43c78dd1ac5e00377f5e_9366.webp",
+    Image2: "store_images/4cb5876afbae4250a787ac5e00365cd3_9366.webp",
+    Image3: "store_images/09c5ea6df1bd4be6baaaac5e003e7047_9366.webp",
+    Image4: "store_images/ff82213b88c74ac5a0cbac5e004bd8e3_9366.webp",
   },
   {
     title: "abdelsalam",
@@ -108,7 +109,7 @@ function showDate(){
     for(let i =0; i < products.length;i++){
         table += `
             <div>
-                <img src="${products[i].Image3}" id="image" alt="Sample photo">
+                <img src="${products[i].Image3}" onmouseenter="adidas(${i})" alt="Sample photo">
                 <div class="text"onclick="wow(i)">
                   <h3>${products[i].title}</h3>
                   <p>${products[i].desc}</p>
@@ -119,6 +120,10 @@ function showDate(){
     }           
     document.getElementById('id').innerHTML = table;
 
+}
+
+function adidas(i){
+this.src = products[i].Image2
 }
 showDate()
 function wow(i){
